b4804e6fafcdd52a7ea8767d8745d0b8c2bb222c
Brian Munro <brian.alexander.munro@gmail.com>
2022-02-22 08:55:22 +0200

    Install Crowdsec OpenResty Bouncer directly into the docker image. Config files and templates are saved in /defaults/crowdsec A script in the main docker image will handle deployment.

--- local-build.sh
+++ local-build.sh
@@ -9,6 +9,7 @@ RESET='\E[0m'
 DOCKER_IMAGE=nginxproxymanager/nginx-full
 
 export OPENRESTY_VERSION=1.19.9.1
+export CROWDSEC_OPENRESTY_BOUNCER_VERSION=0.1.1
 export LUA_VERSION=5.1.5
 export LUAROCKS_VERSION=3.3.1
 
@@ -18,6 +19,7 @@ echo -e "${BLUE}❯ ${CYAN}Building ${YELLOW}latest ${CYAN}...${RESET}"
 docker build \
 	--pull \
 	--build-arg OPENRESTY_VERSION \
+	--build-arg CROWDSEC_OPENRESTY_BOUNCER_VERSION \
 	--build-arg LUA_VERSION \
 	--build-arg LUAROCKS_VERSION \
 	-t ${DOCKER_IMAGE}:latest \
