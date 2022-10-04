bfa318e1f36693f836f3e8ea18aa559d74403abb
baudneo <86508179+baudneo@users.noreply.github.com>
2022-10-03 19:07:19 -0600

    update modsecurity and modsec nginx connector - memory leak issue update crowdsec update OpenResty
    make buildable by users

--- local-build.sh
+++ local-build.sh
@@ -4,25 +4,31 @@ export BLUE='\E[1;34m'
 export CYAN='\E[1;36m'
 export YELLOW='\E[1;33m'
 export GREEN='\E[1;32m'
+export RED='\E[1;31m'
 export RESET='\E[0m'
 
-DOCKER_IMAGE=baudneo/nginx-full
-MAINTAINER="baudneo <baudneo@protonmail.com>"
-REPO_OWNER="baudneo"
-BASE_TAG='cs-modsec'
+export DOCKER_IMAGE=baudneo/nginx-full
+export MAINTAINER="baudneo <baudneo@protonmail.com>"
+export REPO_OWNER="baudneo"
+export BASE_TAG='local_latest'
 
-export OPENRESTY_VERSION=1.19.9.1
+export CERTBOT_NODE_TAG='local_certbot-node'
+export CERTBOT_TAG='local_certbot'
+#export ACMESH_TAG='local_acmesh'
+#export ACMESH_GO_TAG='local_acmesh-golang'
+
+export OPENRESTY_VERSION=1.21.4.1
 export LUA_VERSION=5.1.5
 export LUAROCKS_VERSION=3.3.1
 export NGINX_HTTP_GEOIP2_MODULE_VERSION=3.3
-# needed for GeoIP2 and modsecurity
+# for GeoIP2 and modsecurity
 export MAXMINDDB_VERSION=1.6.0
 # crowdsec openresty bouncer
-export CROWDSEC_OPENRESTY_BOUNCER_VERSION=0.1.0
-#modsecurity
-export MODSECURITY_VERSION=3.0.6
-export MODSECURITY_NGINX_VERSION=1.0.2
-export CRS_VERSION=3.3.2
+export CROWDSEC_OPENRESTY_BOUNCER_VERSION=0.1.11
+# modsecurity
+export MODSECURITY_VERSION=3.0.8
+export MODSECURITY_NGINX_VERSION=1.0.3
+export CRS_VERSION=3.3.4
 
 # Builds
 echo -e "${BLUE}❯ ${CYAN}Building ${YELLOW}${BASE_TAG} ${CYAN}...${RESET}"
@@ -44,40 +50,45 @@ docker build \
         -t ${DOCKER_IMAGE}:${BASE_TAG} \
         -f docker/Dockerfile \
         .
+echo -e "${GREEN}❯❯❯ ${RED}DONE!"
 
-echo -e "${BLUE}❯ ${CYAN}Building ${YELLOW}acmesh ${CYAN}...${RESET}"
+#echo -e "${BLUE}❯ ${CYAN}Building ${YELLOW}acmesh ${CYAN}...${RESET}"
+#docker build \
+#        --build-arg DOCKER_IMAGE="$DOCKER_IMAGE" \
+#        --build-arg MAINTAINER="$MAINTAINER" \
+#        --build-arg BASE_TAG="$BASE_TAG" \
+#        -t ${DOCKER_IMAGE}:acmesh \
+#        -f docker/Dockerfile.acmesh \
+#        .
+echo -e "${BLUE}❯ ${CYAN}Building ${YELLOW}${CERTBOT_TAG} ${CYAN}...${RESET}"
 docker build \
         --build-arg DOCKER_IMAGE="$DOCKER_IMAGE" \
         --build-arg MAINTAINER="$MAINTAINER" \
         --build-arg BASE_TAG="$BASE_TAG" \
-        -t ${DOCKER_IMAGE}:acmesh \
-        -f docker/Dockerfile.acmesh \
-        .
-
-echo -e "${BLUE}❯ ${CYAN}Building ${YELLOW}certbot ${CYAN}...${RESET}"
-docker build \
-        --build-arg DOCKER_IMAGE="$DOCKER_IMAGE" \
-        --build-arg MAINTAINER="$MAINTAINER" \
-        --build-arg BASE_TAG="$BASE_TAG" \
-        -t ${DOCKER_IMAGE}:certbot \
+        --build-arg CERTBOT_TAG="$CERTBOT_TAG" \
+        -t ${DOCKER_IMAGE}:${CERTBOT_TAG} \
         -f docker/Dockerfile.certbot \
         .
+echo -e "${GREEN}❯❯❯ ${RED}DONE!"
+#echo -e "${BLUE}❯ ${CYAN}Building ${YELLOW}acmesh-golang ${CYAN}...${RESET}"
+#docker build \
+#        --build-arg BASE_TAG=acmesh \
+#        --build-arg DOCKER_IMAGE="$DOCKER_IMAGE" \
+#        --build-arg MAINTAINER="$MAINTAINER" \
+#        -t ${DOCKER_IMAGE}:acmesh-golang \
+#        -f docker/Dockerfile.acmesh-golang \
+#        .
 
-echo -e "${BLUE}❯ ${CYAN}Building ${YELLOW}acmesh-golang ${CYAN}...${RESET}"
+echo -e "${BLUE}❯ ${CYAN}Building ${YELLOW}${CERTBOT_NODE_TAG} ${CYAN}...${RESET}"
+echo -e "${RED}CERTBOT_TAG: ${GREEN}${CERTBOT_TAG} ${RED}- BASE_TAG: ${GREEN}${BASE_TAG}${RED} - CERTBOT_NODE_TAG:${GREEN} ${CERTBOT_NODE_TAG} ${RED}- TARGETPLATFORM:${GREEN} ${TARGETPLATFORM} ${RED}- MAINTAINER:${GREEN} ${MAINTAINER}${RESET}"
+
 docker build \
-        --build-arg BASE_TAG=acmesh \
+        --build-arg BASE_TAG="$BASE_TAG" \
         --build-arg DOCKER_IMAGE="$DOCKER_IMAGE" \
         --build-arg MAINTAINER="$MAINTAINER" \
-        -t ${DOCKER_IMAGE}:acmesh-golang \
-        -f docker/Dockerfile.acmesh-golang \
-        .
-
-echo -e "${BLUE}❯ ${CYAN}Building ${YELLOW}certbot-node ${CYAN}...${RESET}"
-docker build \
-        --build-arg BASE_TAG=certbot \
-        --build-arg DOCKER_IMAGE="$DOCKER_IMAGE" \
-        --build-arg MAINTAINER="$MAINTAINER" \
-        -t ${DOCKER_IMAGE}:certbot-node \
+        --build-arg CERTBOT_TAG="$CERTBOT_TAG" \
+        --build-arg CERTBOT_NODE_TAG="$CERTBOT_NODE_TAG" \
+        -t ${DOCKER_IMAGE}:${CERTBOT_NODE_TAG} \
         -f docker/Dockerfile.certbot-node \
         .
 
