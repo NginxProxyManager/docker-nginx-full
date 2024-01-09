dee837c4314715a15d6e5f41e9b5308bf2429d9e
Jamie Curnow <jc@jc21.com>
2024-01-09 14:07:17 +1000

    Use debian bookworm for base
    - Updated node to v20
    - Updated go to 1.21 and deps
    - Updated python, certbot and deps
    - Removed nancy from go image

--- local-buildx.sh
+++ local-buildx.sh
@@ -6,8 +6,9 @@ YELLOW='\E[1;33m'
 GREEN='\E[1;32m'
 RESET='\E[0m'
 
-REGISTRY=${REGISTRY:-}
+REGISTRY=${REGISTRY:-docker.jc21.com/}
 DOCKER_IMAGE="${REGISTRY}jc21/nginx-full"
+PLATFORMS=linux/amd64,linux/arm64,linux/arm/7
 
 export OPENRESTY_VERSION=1.21.4.3
 export CROWDSEC_OPENRESTY_BOUNCER_VERSION=0.1.7
@@ -20,11 +21,20 @@ export CERTBOT_IMAGE="${DOCKER_IMAGE}:certbot"
 export CERTBOT_NODE_IMAGE="${DOCKER_IMAGE}:certbot-node"
 export ACMESH_GOLANG_IMAGE="${DOCKER_IMAGE}:acmesh-golang"
 
+# Setup
+
+docker buildx rm "${BUILDX_NAME:-nginx-full}" || echo
+docker buildx create --name "${BUILDX_NAME:-nginx-full}" || echo
+docker buildx use "${BUILDX_NAME:-nginx-full}"
+
 # Builds
 
 echo -e "${BLUE}❯ ${CYAN}Building ${YELLOW}latest ${CYAN}...${RESET}"
-docker build \
+docker buildx build \
+	--platform "$PLATFORMS" \
+	--progress plain \
 	--pull \
+	--push \
 	--build-arg OPENRESTY_VERSION \
 	--build-arg CROWDSEC_OPENRESTY_BOUNCER_VERSION \
 	--build-arg LUA_VERSION \
@@ -34,31 +44,45 @@ docker build \
 	.
 
 echo -e "${BLUE}❯ ${CYAN}Building ${YELLOW}acmesh ${CYAN}...${RESET}"
-docker build \
+docker buildx build \
+	--platform "$PLATFORMS" \
+	--progress plain \
+	--push \
 	--build-arg BASE_IMAGE \
 	-t "$ACMESH_IMAGE" \
 	-f docker/Dockerfile.acmesh \
 	.
 
 echo -e "${BLUE}❯ ${CYAN}Building ${YELLOW}certbot ${CYAN}...${RESET}"
-docker build \
+docker buildx build \
+	--platform "$PLATFORMS" \
+	--progress plain \
+	--push \
 	--build-arg BASE_IMAGE \
 	-t "$CERTBOT_IMAGE" \
 	-f docker/Dockerfile.certbot \
 	.
 
 echo -e "${BLUE}❯ ${CYAN}Building ${YELLOW}acmesh-golang ${CYAN}...${RESET}"
-docker build \
+docker buildx build \
+	--platform "$PLATFORMS" \
+	--progress plain \
+	--push \
 	--build-arg ACMESH_IMAGE \
 	-t "$ACMESH_GOLANG_IMAGE" \
 	-f docker/Dockerfile.acmesh-golang \
 	.
 
 echo -e "${BLUE}❯ ${CYAN}Building ${YELLOW}certbot-node ${CYAN}...${RESET}"
-docker build \
+docker buildx build \
+	--platform "$PLATFORMS" \
+	--progress plain \
+	--push \
 	--build-arg CERTBOT_IMAGE \
 	-t "$CERTBOT_NODE_IMAGE" \
 	-f docker/Dockerfile.certbot-node \
 	.
+
+docker buildx rm "${BUILDX_NAME:-nginx-full}"
 
 echo -e "${BLUE}❯ ${GREEN}All done!${RESET}"
