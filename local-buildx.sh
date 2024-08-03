709e6c37eb6f4a54641de661d3577eb1cd349415
Dominic Rico Gómez <hello@coderocket.me>
2024-08-03 23:03:32 +0200

    add missing env var for jenkins

--- local-buildx.sh
+++ local-buildx.sh
@@ -13,6 +13,7 @@ export OPENRESTY_VERSION=1.25.3.1
 export CROWDSEC_OPENRESTY_BOUNCER_VERSION=1.0.2
 export LUA_VERSION=5.4.7
 export LUAROCKS_VERSION=3.11.1
+export CSR_VERSION=4.5.0
 
 export BASE_IMAGE="${DOCKER_IMAGE}:latest"
 export ACMESH_IMAGE="${DOCKER_IMAGE}:acmesh"
@@ -38,6 +39,7 @@ docker buildx build \
 	--build-arg CROWDSEC_OPENRESTY_BOUNCER_VERSION \
 	--build-arg LUA_VERSION \
 	--build-arg LUAROCKS_VERSION \
+	--build-arg CSR_VERSION \
 	-t "$BASE_IMAGE" \
 	-f docker/Dockerfile \
 	.
