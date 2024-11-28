6b2fc23fd389eee8f9dd646fa09969966d74450c
mib1185 <mail@mib85.de>
2024-11-28 23:23:08 +0100

    add modsecurity with OWASP core ruleset

--- local-build.sh
+++ local-build.sh
@@ -13,6 +13,9 @@ export OPENRESTY_VERSION=1.25.3.2
 export CROWDSEC_OPENRESTY_BOUNCER_VERSION=0.1.7
 export LUA_VERSION=5.1.5
 export LUAROCKS_VERSION=3.3.1
+export LIBMODSECURITY_VERSION=3.0.13
+export MODSECURITY_NGINX_VERSION=1.0.3
+export CRS_VERSION=4.8.0
 
 export BASE_IMAGE="${DOCKER_IMAGE}:latest"
 export ACMESH_IMAGE="${DOCKER_IMAGE}:acmesh"
@@ -29,6 +32,9 @@ docker build \
 	--build-arg CROWDSEC_OPENRESTY_BOUNCER_VERSION \
 	--build-arg LUA_VERSION \
 	--build-arg LUAROCKS_VERSION \
+	--build-arg LIBMODSECURITY_VERSION \
+	--build-arg MODSECURITY_NGINX_VERSION \
+	--build-arg CRS_VERSION \
 	-t "$BASE_IMAGE" \
 	-f docker/Dockerfile \
 	.
